991e568440753725545bab12cb627fc43aa84d9e
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-09-24 21:08:41 +0700

    Implemented Confirm Reservation form functionality

--- solution.js
+++ solution.js
@@ -11,8 +11,8 @@ let reservation =
 
 function changeContent(className) {
     document.querySelectorAll('.custom-form').forEach(div => div.classList.add('hidden'));
-    if( document.querySelector(`.${className}`) != null){
-    document.querySelector(`.${className}`).classList.remove('hidden');
+    if (document.querySelector(`.${className}`) != null) {
+        document.querySelector(`.${className}`).classList.remove('hidden');
     }
 }
 
@@ -20,4 +20,19 @@ document.querySelector('#new-reservation').addEventListener('click', (e) => clea
 
 function cleanData(e) {
     changeContent('search-form-content');
+}
+
+document.querySelector('#confirm-back-btn').addEventListener('click', (e) => getBackToPersonalData(e));
+
+function getBackToPersonalData(e) {
+    e.preventDefault();
+    changeContent('guest-details-form-content');
+}
+
+
+document.querySelector('#confirm-reservation').addEventListener('click', (e) => showThanksPage(e));
+
+function showThanksPage(e) {
+    e.preventDefault();
+    changeContent('thank-you-content');
 }

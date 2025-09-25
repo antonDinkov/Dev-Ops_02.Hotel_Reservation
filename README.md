0057c03a38e1230266c81d4133b78a881f750067
AndrianStoyanov <ancast@abv.bg>
2025-09-25 23:18:41 +0300

    add confirm form in solution.js, styles.css and make confirm rasarvation.html

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Member Krasimir: Questioner's functionality (Search Form)
 
 Member Nikolay: Offerer's functionality (Our Offers Form) 
 
-Member Andrian: Admin's functionality (Guest Details Form) 
+Member Viliyan: Admin's functionality (Guest Details Form) 
 
 Member Andrian: Verifier's functionality (Confirm Reservation Form) 
--- solution.js
+++ solution.js
@@ -40,3 +40,17 @@ function searchFormData(e) {
         changeContent('search-result-form-content');
     }
 }
+document.querySelector('#confirm-back-btn').addEventListener('click', (e) => getBackToPersonalData(e));
+
+function getBackToPersonalData(e) {
+    e.preventDefault();
+    changeContent('guest-details-form-content');
+}
+
+
+document.querySelector('#confirm-reservation').addEventListener('click', (e) => showThanksPage(e));
+
+function showThanksPage(e) {
+    e.preventDefault();
+    changeContent('thank-you-content');
+}
